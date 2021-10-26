76edf526d69b0bf5cd39ae01ba6e56568c82b8e9
bradcortez23 <brad.cortez23@gmail.com>
2021-10-26 13:59:04 +0800

    Initial commit

---
+++ src/main/java/com/javabasic/redissonbasic/RedissonbasicApplication.java
@@ -0,0 +1,13 @@
+package com.javabasic.redissonbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedissonbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabasic/redissonbasic/RedissonbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabasic.redissonbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
